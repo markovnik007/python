5d5385d7898ebffb06fe59b8343890651b4a7082
markovnik007 <markovnik.007@gmail.com>
2018-07-20 03:27:10 +0700

    Создал функцию создания backup в текущей директории

--- lesson2.py
+++ lesson2.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import shutil
 import psutil	#сторонний
 
 # Коментарий
@@ -11,30 +12,42 @@ print("Привет программист)")
 name = input("Введите ваше имя:")
 print (name, ", добро пожаловать в мир Python!")
 
-answer = input("Поработаем? (Y/N)")
+answer = ""
 #PEP-8 Рекомендации по оформлению кода Python
-if answer == "Y":
-	print("Чем бы ты хотел(а) заняться", name,"?")
-	print("1) Выводим список файлов")
-	print("2) Выводим информацию о системе")
-	print("3) Выводим список процессов")
-	choice = int(input("Что будем делать?"))
-	
-	if choice == 1:
-		print(os.listdir())
-	elif choice == 2:
-		print("Информация о пользователе:", os.getlogin())
-		print("Текущая директория", os.getcwd())
-		print("Платформа ОС:", sys.platform)
-		print("Количество CPU:", os.cpu_count())
-	elif choice == 3:
-		print(psutil.pids())
+while answer !="q":
+	answer = input("Поработаем? (Y/N/q)")
+	if answer == "Y":
+		print("Чем бы ты хотел(а) заняться", name,"?")
+		print("1) Выводим список файлов")
+		print("2) Выводим информацию о системе")
+		print("3) Выводим список процессов")
+		print("4) Продублирую файлы в текущей директории")
+		choice = int(input("Что будем делать?"))
+		
+		if choice == 1:
+			print(os.listdir())
+		elif choice == 2:
+			print("Информация о пользователе:", os.getlogin())
+			print("Текущая директория", os.getcwd())
+			print("Платформа ОС:", sys.platform)
+			print("Количество CPU:", os.cpu_count())
+		elif choice == 3:
+			print(psutil.pids())
+		elif choice == 4:
+			print("Создание бэкапов в текущей директории")
+			file_list = os.listdir()
+			i = 0
+			while i < len(file_list):
+				if os.path.isfile(file_list[i]):
+					newfile = file_list[i] + ".bak"
+					shutil.copy(file_list[i], newfile)
+				i = i + 1
+		else:
+			pass
 	else:
 		pass
-else:
-	pass
-if answer == "N":
-	print("Давай досвидания", name,"!")
-else:
-	pass
+	if answer == "N":
+		print("Давай досвидания", name,"!")
+	else:
+		pass
 	
